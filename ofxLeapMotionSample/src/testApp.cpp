005753a3b820db83969ee1227eda66856cbaf02a
LiJing <lijing@3-dj.com>
2013-05-15 22:07:11 +0800

    add win proj

---
+++ ofxLeapMotionSample/src/testApp.cpp
@@ -0,0 +1,74 @@
+#include "testApp.h"
+#include "LeapMotion.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    // Create a sample listener and controller
+    SampleListener listener;
+    Controller controller;
+
+    // Have the sample listener receive events from the controller
+    controller.addListener(listener);
+
+    // Keep this process running until Enter is pressed
+    std::cout << "Press Enter to quit..." << std::endl;
+    std::cin.get();
+
+    // Remove the sample listener when done
+    controller.removeListener(listener);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
